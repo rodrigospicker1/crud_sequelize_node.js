7d76144fcacc5273e25e0f717759b5474d3e6c3c
rodrigospicker1 <r.spicker13@gmail.com>
2023-03-03 12:55:46 -0300

    select_delete

--- index.js
+++ index.js
@@ -22,8 +22,31 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await User.findOne({raw: true, where: {id: id}})
+
+    res.render('userview', {user})
+})
+
+app.post('/users/delete/:id', async (req, res) => {
+    const id = req.params.id
+
+    await User.destroy({where: {id: id}})
+
+    res.redirect('/')
+})
+
+app.get('/users/edit/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await User.findOne({raw: true, where: {id: id}})
+
+    res.render('useredit', {user})
+})
+
+app.get('/', async (req, res) => {
+    const users = await User.findAll({raw: true})
+    res.render('home', {users: users})
 })
 
 app.get('/users/create', (req,res) => {
@@ -31,6 +54,7 @@ app.get('/users/create', (req,res) => {
 })
 
 app.post('/users/create', async (req,res) => {
+    
     const name = req.body.name
     const occupation = req.body.occupation
     let newsletter = req.body.newsletter
