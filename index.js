cbc6a4e73fb3df21d399c65fb6fb6a75afed0bfc
rodrigospicker1 <r.spicker13@gmail.com>
2023-03-04 00:54:40 -0300

    relation_table

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 
 const User = require('./models/User')
+const Address = require('./models/Address')
 
 const app = express()
+
 conn.sync().then(() => {
     app.listen(3000)
 }).catch((err) => console.log(err))
@@ -21,6 +23,24 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+app.get('/users/create', (req,res) => {
+    res.render('adduser')
+})
+
+
+app.post('/address/create', async (req, res) => {
+    const UserId = req.body.UserId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+    
+    const address = {UserId, street, number, city}
+
+    await Address.create(address)
+
+    res.redirect(`/users/edit/${UserId}`)
+})
 
 app.get('/users/:id', async (req, res) => {
     const id = req.params.id
@@ -39,18 +59,48 @@ app.post('/users/delete/:id', async (req, res) => {
 
 app.get('/users/edit/:id', async (req, res) => {
     const id = req.params.id
-    const user = await User.findOne({raw: true, where: {id: id}})
+    const user = await User.findOne({include: Address, where: {id: id}})
 
-    res.render('useredit', {user})
+    res.render('useredit', {user: user.get({plain: true})})
+})
+
+app.post('/users/update', async (req,res) => {
+    
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on'){
+        newsletter = true
+    }else{
+        newsletter = false
+    }
+
+    const userData = {
+        id, name, occupation, newsletter
+    }
+
+    await User.update(userData, {where: {id: id}})
+
+    res.redirect('/')
+})
+
+app.post('/address/delete', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+    console.log(UserId)
+
+    await Address.destroy({
+        where: {id: id}
+    })
+
+    res.redirect(`/users/edit/${UserId}`)
 })
 
 app.get('/', async (req, res) => {
     const users = await User.findAll({raw: true})
-    res.render('home', {users: users})
-})
-
-app.get('/users/create', (req,res) => {
-    res.render('adduser')
+    res.render('home', {users})
 })
 
 app.post('/users/create', async (req,res) => {
